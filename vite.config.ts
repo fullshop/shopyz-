83bbe2d6c3d495460d7ff4edd3e63cd9921634bc
fullshop <irsam8552@gmail.com>
2026-01-02 21:03:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shopyZ-/', // This must match your GitHub repository name
+  base: '/shopyz-/', // This must match your GitHub repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
