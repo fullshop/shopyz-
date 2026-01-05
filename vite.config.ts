5d40f131c40816fa8e17722adf2a841b4724ef60
fullshop <irsam8552@gmail.com>
2026-01-05 01:57:20 +0100

    Refactor path alias and base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shopyz-/', // Matches your repo name exactly
+  base: '/shopyz-/', // This MUST match your repository name
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, '.'),
+    }
   },
   build: {
     outDir: 'dist',
